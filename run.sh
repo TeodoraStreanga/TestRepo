7f45ba0db2583b9358c6866773d6cb5b2bde4064
TeodoraStreanga <95693614+TeodoraStreanga@users.noreply.github.com>
2022-11-17 18:16:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-DIR="Test"
-[ ! -d "$DIR" ] && mkdir "$DIR"
-sudo chmod +x /var/www/html/
-sudo mv -v ~/"$DIR"/* /var/www/html/
+DIR="/home/ubuntu/Test"
+if [ ! -d "$DIR" ]; then
+  sudo chmod +x /var/www/html/
+  sudo mv -v "$DIR"/* /var/www/html/
+fi
