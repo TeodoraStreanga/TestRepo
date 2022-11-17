4af976bf8a3c30e90a424bcef448c5858e703334
TeodoraStreanga <95693614+TeodoraStreanga@users.noreply.github.com>
2022-11-17 17:36:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 if [-d Test]; then
   sudo rm -r -d Test
 else
-sudo mkdir Test
-cd Test
-sudo chmod +x /var/www/html/
-sudo mv -f * /var/www/html/
+  sudo mkdir Test
+  cd Test
+  sudo chmod +x /var/www/html/
+  sudo mv -f * /var/www/html/
 fi
