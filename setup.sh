1facf50aabe912de9a6dd09b365a6e700f5c9b54
TeodoraStreanga <95693614+TeodoraStreanga@users.noreply.github.com>
2022-11-17 16:39:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install -y apache2
-systemctl start apache2
-systemctl enable apache2
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
