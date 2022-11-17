14b2a728d69bbff29ff37b911aba290e63674f2a
TeodoraStreanga <95693614+TeodoraStreanga@users.noreply.github.com>
2022-11-17 17:51:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DIR="/Test/"
+DIR="/Test"
 if [ -d "$DIR" ]; then
   sudo rm -r -d "$DIR"
 else
-  sudo mkdir Test
-  cd Test
+  sudo mkdir -p "$DIR"
+  cd "$DIR"
   sudo chmod +x /var/www/html/
   sudo mv -f * /var/www/html/
 fi
